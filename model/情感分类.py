c8c85da1a3799bbcc1efa882ba4e99fb24fc9eb8
GodOdimm <63182936+GodOdimm@users.noreply.github.com>
2021-03-31 13:45:09 +0800

    Update 情感分类.py

--- model/情感分类.py
+++ model/情感分类.py
@@ -13,14 +13,12 @@ import tensorflow as tf
 from coef import TRAIN_COEF, PATH
 
 
-def my_normalize(tmp):
-    a = tmp
-    colmax = np.max(a, axis=0)
-    for i in range(140):
-        a[:, i] = a[:, i] / colmax[i]
-    return a
-
-
+# def my_normalize(tmp):
+#     a = tmp
+#     colmax = np.max(a, axis=0)
+#     for i in range(107):
+#         a[:, i] = a[:, i] / colmax[i]
+#     return a
 def read_data(file_name, topPath):
     count = 0
     num1, num2 = TRAIN_COEF['shape']
@@ -34,11 +32,10 @@ def read_data(file_name, topPath):
             for row in reader:
                 if i >= num1:
                     break
-                gg = np.array(row[0:140], dtype='float64')
+                gg = np.array(row[:], dtype='float64')
                 tmp[i, :] = gg
                 i += 1
         # 归一化
-        tmp = my_normalize(tmp)
         data[di,] = tmp[:, :]
         di += 1
         count = count + 1
@@ -49,7 +46,7 @@ def read_data(file_name, topPath):
 
 def get_train_betch(X_train, y_train, batch_size, topPath):
     x_data = None
-    maxNum = 900
+    maxNum = 2500
     x_len = len(X_train)
     if len(X_train) > maxNum:
         x_data = read_data(X_train[:maxNum], topPath)
@@ -89,7 +86,7 @@ if __name__ == '__main__':
     time_start = time.time()
     song_name = []
     song_label = []
-    with open('out/myEmotion.csv') as f:
+    with open('myEmotionDEAM.csv') as f:
         reader = csv.reader(f)
         for row in reader:
             song_name.append(row[0])
@@ -98,7 +95,7 @@ if __name__ == '__main__':
     # print(song_name[0:20])
     # 数据集划分 8:2
     board = len(song_name)
-    # board=200
+    # board = 500
     print("song number=%d" % board)
     song_label = np_utils.to_categorical(song_label)
     X_train, X_test, y_train, y_test = train_test_split(song_name[:board], song_label[:board], test_size=0.2,
@@ -108,70 +105,64 @@ if __name__ == '__main__':
     # batch_size = len(X_train)
     # print(batch_size)
 
+    # 防止宕机的一些设置
+    gpu_devices = tf.config.experimental.list_physical_devices('GPU')
+    for device in gpu_devices:
+        tf.config.experimental.set_memory_growth(device, True)
+
     # dataX = read_data(X_train, topPath)  # 读取
     # X = np.reshape(dataX, (len(dataX), 3600, 107))
 
-    a = input("是否使用模型? : 1:0")
-    if a == '0':
-        y = y_train
-        # 搭建网络
-        model = Sequential()
-        num1, num2 = TRAIN_COEF['shape']
-        model.add(LSTM(100, input_shape=TRAIN_COEF['shape']))
-        # model.add(LSTM(32, input_shape=(3600, 107)))
-        # model.add(LSTM(100, batch_input_shape=(batch_size, 3800,107), return_sequences=True))
-        # model.add(LSTM(100, batch_input_shape=(batch_size, num1,num2), stateful=True))
-        # model.add(LSTM(16, batch_input_shape=(batch_size, X.shape[1], X.shape[2]), return_sequences=True))
-        model.add(Dense(2, activation='softmax'))
-        # sgd = keras.optimizers.SGD(lr=0, decay=1e-6, momentum=0.9, nesterov=True)
-        KerasOptimizer = keras.optimizers.RMSprop(lr=0, rho=0.9, epsilon=1e-08, decay=0.0)  # last is 0.001
-        model.compile(loss='categorical_crossentropy', optimizer=KerasOptimizer, metrics=['accuracy'])
-        # model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-        # it actually work
-        model.fit(get_train_betch(X_train, y_train, batch_size=batch_size, topPath=PATH['EMO_FEATURES_PATH']),
-                  epochs=TRAIN_COEF['epoch'],
-                  verbose=2, steps_per_epoch=int(len(X_train) / batch_size))
+    y = y_train
+    # 搭建网络
+    model = Sequential()
+    num1, num2 = TRAIN_COEF['shape']
+    model.add(LSTM(64, input_shape=TRAIN_COEF['shape'], return_sequences=True))
+    # model.add(LSTM(32))
+    # model.add(LSTM(32, input_shape=(3600, 107)))
+    # model.add(LSTM(100, batch_input_shape=(batch_size, 3800,107), return_sequences=True))
+    # model.add(LSTM(100, batch_input_shape=(batch_size, num1,num2), stateful=True))
+    # model.add(LSTM(16, batch_input_shape=(batch_size, X.shape[1], X.shape[2]), return_sequences=True))
+    model.add(Dense(2, activation='softmax'))
+    # KerasOptimizer = keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0) #last is 0.001
+    # model.compile(loss='categorical_crossentropy', optimizer=KerasOptimizer, metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-        # model.fit(X, y, epochs=epoch, batch_size=1, verbose=2)
+    # it actually work
+    model.fit(get_train_betch(X_train, y_train, batch_size=batch_size, topPath=PATH['DEAM_FEATURE_PATH']),
+              epochs=TRAIN_COEF['epoch'],
+              verbose=1, steps_per_epoch=int(len(X_train) / batch_size))
 
-        # for i in range(epoch):
-        #     model.fit(X, y, epochs=1, batch_size=batch_size, verbose=2)
-        #     model.reset_states()
+    # X1=read_data(X_train, PATH['DEAM_FEATURE_PATH'])
+    # X=np.reshape(X1,(len(X1),num1,num2))
+    # model.fit(X, y, epochs=TRAIN_COEF['epoch'], batch_size=TRAIN_COEF['batch_size'], verbose=1)
 
-        # result = model.fit_generator(generator=get_train_betch(X_train, y_train, batch_size=batch_size, topPath=topPath),
-        #                                  verbose=2,epochs=20)
+    # for i in range(epoch):
+    #     model.fit(X, y, epochs=1, batch_size=batch_size, verbose=2)
+    #     model.reset_states()
 
-        # 保存模型
-        print("Saving model to disk \n")
-        mp = "/Users/chenfeng/Desktop/IAE/emotion/emotion_predict.h5"
-        model.save(mp)
+    # result = model.fit_generator(generator=get_train_betch(X_train, y_train, batch_size=batch_size, topPath=topPath),
+    #                                  verbose=2,epochs=20)
 
-        # 评估
-        dataX_test = read_data(X_test, PATH['EMO_FEATURES_PATH'])
-        x1, x2 = TRAIN_COEF['shape']
-        dataX_test = np.reshape(dataX_test, (len(dataX_test), x1, x2))
-        datay_test = y_test
+    # 保存模型
+    print("Saving model to disk \n")
+    mp = "D:\\DataSet\\model\\tmp.h5"
+    model.save(mp)
 
-        scores = model.evaluate(dataX_test, datay_test, batch_size=batch_size, verbose=0)
-        model.reset_states()
-        print("Model Accuracy: %.2f%%" % (scores[1] * 100))
+    # 评估
+    dataX_test = read_data(X_test, PATH['DEAM_FEATURE_PATH'])
+    x1, x2 = TRAIN_COEF['shape']
+    dataX_test = np.reshape(dataX_test, (len(dataX_test), x1, x2))
+    datay_test = y_test
 
-        # 打印运行时间
-        time_end = time.time()
-        import datetime
+    scores = model.evaluate(dataX_test, datay_test, verbose=0)
+    # model.reset_states()
+    print("Model Accuracy: %.2f%%" % (scores[1] * 100))
 
-        k = datetime.timedelta(seconds=int((time_end - time_start)))
-        print('cost time:', k)
-    else:  # a='1':
-        from keras.models import load_model
+    # 打印运行时间
+    time_end = time.time()
+    import datetime
 
-        model = load_model("/Users/chenfeng/Desktop/IAE/emotion/emotion_predict.h5")
-        # 评估
-        dataX_test = read_data(X_test, PATH['EMO_FEATURES_PATH'])
-        x1, x2 = TRAIN_COEF['shape']
-        dataX_test = np.reshape(dataX_test, (len(dataX_test), x1, x2))
-        datay_test = y_test
-        for i in range(len(X_test)):
-            k = np.reshape(dataX_test[i], (1, x1, x2))
-            tmp = model.predict(k)
-            print(X_test[i], tmp,datay_test[i])
+    k = datetime.timedelta(seconds=int((time_end - time_start)))
+    print('cost time:', k)
+
